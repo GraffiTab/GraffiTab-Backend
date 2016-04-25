13eb111a2410dc3c6b3281d937bb3033a7f4b849
lucia <lucia_c_l@hotmail.com>
2016-04-25 19:33:18 +0100

    Explanatory text for user validation errors.

--- src/main/java/com/graffitab/server/service/user/UserValidationService.java
+++ src/main/java/com/graffitab/server/service/user/UserValidationService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import com.graffitab.server.api.errors.RestApiException;
+import com.graffitab.server.api.errors.ResultCode;
 import com.graffitab.server.persistence.model.user.User;
 
 @Service
@@ -18,32 +20,52 @@ public class UserValidationService {
 	public Boolean validateUser(User user) {
 
 		boolean validationResult = false;
+		String errorText = "";
 		
 		if ( StringUtils.isEmpty(user.getUsername()) || StringUtils.isEmpty(user.getEmail()) ||
 			 StringUtils.isEmpty(user.getFirstName()) || StringUtils.isEmpty(user.getLastName()) ||
 			 StringUtils.isEmpty(user.getPassword())) {
 
 			validationResult = false;
-
+			errorText = "Empty mandatory field." ;
+			
 		} else if (!user.getUsername().matches("[A-Za-z0-9-_]+") ||
-				    user.getUsername().length() <= 3 ||
+				    user.getUsername().length() < 3 ||
 				    user.getUsername().length() > 25){
 			
 			validationResult = false;
+			errorText = "Invalid username";
 			
 		} else if(!user.getEmail().matches("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")){
 			
 			validationResult = false;
+			errorText = "Invalid email";
 			
 		} else {
 
-			if (isUsernameTaken(user.getUsername(), user.getId()) || isEmailTaken(user.getEmail(), user.getId()) ){
+			if (isUsernameTaken(user.getUsername(), user.getId())  ){
+				
 				validationResult = false;
-			} else {
+				errorText="Username already in use.";
+				
+			} else if(isEmailTaken(user.getEmail(), user.getId())){
+				
+				validationResult = false;
+				errorText="Email already in use.";
+				
+			} else { 
+				
 				validationResult = true;
+				
 			}
 		}
-
+		
+		if (!validationResult) {
+			
+			throw new RestApiException(ResultCode.BAD_REQUEST, errorText);
+			
+		}
+		
 		return validationResult;
 	}
 
